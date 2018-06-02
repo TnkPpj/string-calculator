9f26b31649e5b8c910ef9b71cd7252f8111544c8
Ko_n P <tnk.ppj6@gmail.com>
2018-06-02 16:01:40 +0700

    fix

--- Calculator.js
+++ Calculator.js
@@ -1,7 +1,6 @@
-class Calculator{
-    calculate(str){
-        if(str=='')
-            return 0;
-    }
+class Calculator {
+  calculate(str) {
+    if (str === '') { return 0; }
+  }
 }
 module.exports = Calculator;
